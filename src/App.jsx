f9e681188f172962fcfe7d33ac9bf1b174455217
Aizaz <aizazk0309@protonmail.com>
2023-12-20 14:07:44 +0530

    feat: add a simple context

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-
-import './App.css'
+import { useContext, useState } from "react";
+import "./App.css";
+import UserContext from "./context/userContext";
 
 function App() {
-  
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, setUser } = useContext(UserContext);
+  console.log("🚀 ~ file: App.jsx:9 ~ App ~ setUser:", setUser)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    setUser({
+      username,
+      password,
+    });
+  };
   return (
     <>
-      
+      <form action="">
+        <div>
+          <label htmlFor="username">username:</label>
+          <input
+            type="text"
+            name="username"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">password:</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button onClick={handleSubmit}>submit</button>
+      </form>
+      <div>
+        <h1>{user?.username}</h1>
+        <p>{user?.password && "user logged in."}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
